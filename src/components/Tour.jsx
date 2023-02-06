4a5ce80ddb3ec44696f33170906f10ce59eb586b
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-02-06 20:27:40 -0300

    creating container component

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
+import styled from 'styled-components'
+
+const Container = styled.article`
+  background: #fff;
+  border-radius: 0.25rem;
+  margin: 2rem 0;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s linear;
+  &:hover {
+    box-shadow: var(--dark-shadow);
+  }
+
+  img {
+  width: 100%;
+  height: 20rem;
+  object-fit: cover;
+  border-top-right-radius: 0.25rem;
+  border-top-left-radius: 0.25rem;
+  }
+
+  footer {
+  padding: 1.5rem 2rem;
+  }
+`
+
 
 const Tour = ({ tour, removeTour }) => {
   const { id, image, info, name, price } = tour
   const [readMore, setReadMore] = useState(false)
 
   return (
-    <article>
+    <Container>
       <img src={image} alt={name} />
       <footer>
         <div>
-          <h4>{name}</h4>
+          <h3>{name}</h3>
           <h4>$ {price}</h4>
         </div>
         <p>
@@ -20,7 +45,7 @@ const Tour = ({ tour, removeTour }) => {
         </p>
         <button onClick={() => removeTour(id)}>Delete</button>
       </footer>
-    </article>
+    </Container>
   )
 }
 
