66650e8f017f06a2e50637e6eff870f194af0134
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-02-07 16:29:19 -0300

    creating headers components

---
+++ src/components/Headers.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Headers = styled.div`
+    display: block;
+    width: 300px;
+    margin: 1rem auto 0 auto;
+    color: hsl(205, 78%, 60%);
+    letter-spacing: 0.1rem;
+    padding: 0.25rem 0.5rem;
+    text-align: center;
+`
+    
+export default Headers
