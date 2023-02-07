cd9fd8cfec0c2eeb227eafa7966feea99bbbb9cf
Gustavo Rodrigues <Gustavorr001@gmail.com>
2023-02-07 16:48:26 -0300

    creating refresh button styling

---
+++ src/components/Refresh.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const RefreshBtn = styled.button`
+    background: hsl(205, 78%, 60%);
+    display: inline-block;
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.25rem;
+    text-transform: capitalize;
+    color: #fff;
+    letter-spacing: 0.1rem;
+    border-color: transparent;
+    cursor: pointer;
+    margin-top: 2rem;
+    font-size: 1.2rem;
+`
